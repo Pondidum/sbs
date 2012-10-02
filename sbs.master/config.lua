01f9285094059736816f4f6300a8a969ea1af583
andy <andy@stormbase.net>
2012-10-02 22:03:09 +0100

    config: added missing commas

--- sbs.master/config.lua
+++ sbs.master/config.lua
@@ -7,4 +7,9 @@ ns.config = {
 		unranked = 2.25,
 	},
 	minbid = 3,
+	boundaries = {
+		[1] = { points = 50,  factor = 1 	   },
+		[2] = { points = 100, factor = (2 / 3) },
+		[3] = { points = 150, factor = (1 / 3) },
+	}
 }
